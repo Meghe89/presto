237834598fe86b76dfc902a305020f7a3e1e77c2
Michele Cozzola <cozzolamichele89@gmail.com>
2021-10-02 11:45:23 +0200

    categories

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 /* collegamenti */
 const navbar = document.querySelector('#nav-presto')
 const toggler = document.querySelector('.navbar-toggler')
+const categoriesWrapper = document.querySelector('#categories-wrapper')
+
+
+
+/* array categorie */
+let categories = [
+    {'name' : 'Moto' , icon : 'fas fa-motorcycle'},
+    {'name' : 'Auto' , icon : 'fas fa-car'},
+    {'name' : 'Bici' , icon : 'fas fa-bicycle'},
+    {'name' : 'Telefono' , icon : 'fas fa-mobile'},
+    {'name' : 'Computer' , icon : 'fas fa-laptop'},
+    {'name' : 'Gaming' , icon : 'fas fa-gamepad'},
+    {'name' : 'Immobili' , icon : 'fas fa-home'},
+    {'name' : 'Guitars' , icon : 'fas fa-guitar'},
+]
+
+
+/* creazione elementi  */
+
+categories.forEach(category=>{
+    console.log(category);
+    let card = document.createElement('div')
+    card.classList.add('col-12', 'col-md-6', 'col-lg-3','d-flex', 'justify-content-center')
+    card.innerHTML = 
+    `
+    <div class="card-category tc-white text-center my-3">
+        <h3 class="fw-bold mb-4 tc-main"> <i class="${category.icon}"></i> ${category.name}</h3>
+        <button class="btn-main">Vai alla categoria</button>
+    </div>
+    `
+    categoriesWrapper.appendChild(card)
+})
 
 /* event */
+// cambio colore navbar
 document.addEventListener('scroll', ()=>{
-    console.log(window.scrollY);
+    
     if (window.scrollY > 100) {
         navbar.classList.add('bg-white')
     } else{
@@ -12,6 +45,8 @@ document.addEventListener('scroll', ()=>{
     }
 })
 
+//hamburger ruota
 toggler.addEventListener('click', ()=>{
     toggler.classList.toggle('fa-rotate-90')
 })
+
